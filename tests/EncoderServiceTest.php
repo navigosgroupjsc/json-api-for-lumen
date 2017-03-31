c7b47bb5cd94bfa6d5133a9d6d6381071a5dd3b5
Jason Silkey <jason.silkey@realpage.com>
2017-03-31 08:36:30 -0700

    add test cases for encoder options

--- tests/EncoderServiceTest.php
+++ tests/EncoderServiceTest.php
@@ -58,4 +58,53 @@ class EncoderServiceTest extends \PHPUnit\Framework\TestCase
         $encoder = $this->encoder_service->getEncoder();
         $this->assertSame($encoder, $this->encoder_service->getEncoder());
     }
+
+    public function testGetEncoderOptionsDefaults()
+    {
+        $method = $this->getMethod(EncoderService::class, 'getEncoderOptions');
+
+        $service = new EncoderService([]);
+
+        $encoder_options = $method->invokeArgs($service, [[]]);
+
+        $this->assertEquals(0, $encoder_options->getOptions());
+        $this->assertNull($encoder_options->getUrlPrefix());
+        $this->assertEquals(512, $encoder_options->getDepth());
+    }
+
+    public function testGetEncoderOptions()
+    {
+        $method = $this->getMethod(EncoderService::class, 'getEncoderOptions');
+
+        $service = new EncoderService([]);
+
+        $configs = [
+            [
+                'options' => 0,
+                'urlPrefix' => null,
+                'depth' => 512
+            ],
+            [
+                'options' => JSON_PRETTY_PRINT,
+                'urlPrefix' => '/',
+                'depth' => 1024
+            ],
+        ];
+
+        foreach ($configs as $config) {
+            $encoder_options = $method->invokeArgs($service, [$config]);
+
+            $this->assertEquals($config['options'], $encoder_options->getOptions());
+            $this->assertEquals($config['urlPrefix'], $encoder_options->getUrlPrefix());
+            $this->assertEquals($config['depth'], $encoder_options->getDepth());
+        }
+    }
+
+    protected static function getMethod($class, $name)
+    {
+        $class  = new \ReflectionClass($class);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method;
+    }
 }
