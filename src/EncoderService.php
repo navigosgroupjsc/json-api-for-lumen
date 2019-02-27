d2a0133c4f28c98fdf8c536f5293d1bd94d31efc
Tuan Phan <tuan.phan@navigosgroup.com>
2019-02-27 14:06:28 +0700

    Fixed neomerx/json-api version 3.* back-compatible breaking

--- src/EncoderService.php
+++ src/EncoderService.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace RealPage\JsonApi;
 
+use Neomerx\JsonApi\Contracts\Encoder\EncoderInterface;
 use Neomerx\JsonApi\Encoder\Encoder;
-use Neomerx\JsonApi\Encoder\EncoderOptions;
 
 /**
  * Class EncoderService
@@ -31,7 +31,7 @@ class EncoderService
         if (!isset($this->encoders[$name])) {
             if ($name === 'default') {
                 $config = $this->config;
-            } elseif(isset($this->config['encoders'][$name])) {
+            } elseif (isset($this->config['encoders'][$name])) {
                 $config = $this->config['encoders'][$name];
             } else {
                 throw new \Exception(sprintf('No configuration found for %s "%s"', Encoder::class, $name));
@@ -40,18 +40,17 @@ class EncoderService
             $encoder_options = isset($config['encoder-options']) && is_array($config['encoder-options']) ?
                 $config['encoder-options'] :
                 [];
-            $options = $this->getEncoderOptions($encoder_options);
 
             $encoder = Encoder::instance(
-                $this->config['schemas'],
-                $options
+                $this->config['schemas']
             );
+            $encoder = empty($encoder_options) ? $encoder : $this->makeEncoderOptions($encoder, $encoder_options);
 
             if (isset($config['jsonapi'])) {
                 if (is_array($config['jsonapi'])) {
                     $encoder->withJsonApiVersion($config['jsonapi']);
                 } elseif ($config['jsonapi'] === true) {
-                    $encoder->withJsonApiVersion();
+                    $encoder->withJsonApiVersion(Encoder::JSON_API_VERSION);
                 }
             }
             if (isset($config['meta']) && is_array($config['meta'])) {
@@ -60,20 +59,27 @@ class EncoderService
 
             $this->encoders[$name] = $encoder;
         }
+
         return $this->encoders[$name];
     }
 
-    protected function getEncoderOptions(array $config)
+    protected function makeEncoderOptions(EncoderInterface $encoder, array $config)
     {
         $options = isset($config['options']) && is_int($config['options']) ?
             $config['options'] :
             0;
+        $encoder->withEncodeOptions($options);
+
         $urlPrefix = isset($config['urlPrefix']) && is_string($config['urlPrefix']) ?
             $config['urlPrefix'] :
-            null;
+            Encoder::DEFAULT_URL_PREFIX;
+        $encoder->withUrlPrefix($urlPrefix);
+
         $depth = isset($config['depth']) && is_int($config['depth']) ?
             $config['depth'] :
             512;
-        return new EncoderOptions($options, $urlPrefix, $depth);
+        $encoder->withEncodeDepth($depth);
+
+        return $encoder;
     }
 }
